a11ce8b391c96fbf34a95444da28c0945d66dcc2
Rusfighter <ilija.lisenkov@gmail.com>
2011-01-25 20:26:32 +0100

    Hack for Donation Rewarder

--- src/server/scripts/Custom/donationrewarder.cpp
+++ src/server/scripts/Custom/donationrewarder.cpp
@@ -18,7 +18,7 @@ class donationrewarder : public CreatureScript
         void Reward(Player* pPlayer, Creature* pCreature, int item , int count, int cost, int type = 3, int32 spell = 0)
         {
             QueryResult result;
-            result = CharacterDatabase.PQuery("SELECT dp FROM auth.account WHERE id = '%u'", pPlayer->GetSession()->GetAccountId());
+            result = LoginDatabase.PQuery("SELECT dp FROM account WHERE id = '%u'", pPlayer->GetSession()->GetAccountId());
             char str[200];
             if (!result) // check
             {
@@ -43,7 +43,7 @@ class donationrewarder : public CreatureScript
                     }
                     else
                     {
-                        CharacterDatabase.PQuery("Update auth.account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
+                        LoginDatabase.PQuery("Update account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
                         sprintf(str,"Your points are taken, Please Relog!!!!");
                         pPlayer->MonsterWhisper(str,pPlayer->GetGUID(),true);
                         pPlayer->SetAtLoginFlag(AT_LOGIN_RENAME);
@@ -68,7 +68,7 @@ class donationrewarder : public CreatureScript
                     {
                         if (pPlayer->AddItem(item, count))
                         {
-                            CharacterDatabase.PQuery("Update auth.account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
+                            LoginDatabase.PQuery("Update account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
                             sprintf(str,"Your points are taken and your item is given!!!");
                             pPlayer->MonsterWhisper(str,pPlayer->GetGUID(),true);
                         }
@@ -89,7 +89,7 @@ class donationrewarder : public CreatureScript
                     }
                     else
                     {
-                        CharacterDatabase.PQuery("Update auth.account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
+                        LoginDatabase.PQuery("Update account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
                         sprintf(str,"Your points are taken, Please Relog!!!!");
                         pPlayer->MonsterWhisper(str,pPlayer->GetGUID(),true);
                         pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
@@ -105,7 +105,7 @@ class donationrewarder : public CreatureScript
                     }
                     else
                     {
-                        CharacterDatabase.PQuery("Update auth.account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
+                        LoginDatabase.PQuery("Update account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
                         sprintf(str,"Your points are taken, Please Relog!!!!");
                         pPlayer->MonsterWhisper(str,pPlayer->GetGUID(),true);
                         pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
@@ -121,7 +121,7 @@ class donationrewarder : public CreatureScript
                     }
                     else
                     {
-                        CharacterDatabase.PQuery("Update auth.account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
+                        LoginDatabase.PQuery("Update account Set dp = dp - '%u' WHERE id = '%u'", cost, pPlayer->GetSession()->GetAccountId());
                         sprintf(str,"Your points are taken!!");
                         pPlayer->MonsterWhisper(str,pPlayer->GetGUID(),true);
                         pPlayer->CastSpell(pPlayer, spell, true);
@@ -132,11 +132,8 @@ class donationrewarder : public CreatureScript
                  
 
             }
-
             pPlayer->PlayerTalkClass->ClearMenus();
-            return;
-
-            //OnGossipHello(pPlayer, pCreature);
+            OnGossipHello(pPlayer, pCreature);
         }
 
         bool OnGossipHello(Player* pPlayer, Creature* pCreature)
@@ -144,8 +141,8 @@ class donationrewarder : public CreatureScript
             pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, "Howmany donation points do i have?", GOSSIP_SENDER_MAIN, 1000);
             pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Genetic Modifier", GOSSIP_SENDER_MAIN, 2000);
             pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Rings/Trinkets", GOSSIP_SENDER_MAIN, 3000);
-            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, "1 x Donation Coin - Cost 5 DP", GOSSIP_SENDER_MAIN, 4000);
-            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, "4 x Donation Coin - Cost 18 DP", GOSSIP_SENDER_MAIN, 4001);
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, "1 x Donation Coin - Cost 5 dp", GOSSIP_SENDER_MAIN, 4000);
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, "4 x Donation Coin - Cost 18 dp", GOSSIP_SENDER_MAIN, 4001);
             pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Other Stuff", GOSSIP_SENDER_MAIN, 5000);
             
 
@@ -165,14 +162,14 @@ class donationrewarder : public CreatureScript
                 break;
             case 2000:
                 pPlayer->PlayerTalkClass->ClearMenus();
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Human Male - Cost 1 DP", GOSSIP_SENDER_MAIN, 2001);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Human Female - Cost 1 DP", GOSSIP_SENDER_MAIN, 2002);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Gnome Male - Cost 1 DP", GOSSIP_SENDER_MAIN, 2003);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Gnome Female - Cost 1 DP", GOSSIP_SENDER_MAIN, 2004);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Tauren Male - Cost 1 DP", GOSSIP_SENDER_MAIN, 2005);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Tauren Female - Cost 1 DP", GOSSIP_SENDER_MAIN, 2006);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Blood Elf Male - Cost 1 DP", GOSSIP_SENDER_MAIN, 2007);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Blood Elf Female - Cost 1 DP", GOSSIP_SENDER_MAIN, 2008);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Human Male - Cost 1 dp", GOSSIP_SENDER_MAIN, 2001);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Human Female - Cost 1 dp", GOSSIP_SENDER_MAIN, 2002);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Gnome Male - Cost 1 dp", GOSSIP_SENDER_MAIN, 2003);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Gnome Female - Cost 1 dp", GOSSIP_SENDER_MAIN, 2004);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Tauren Male - Cost 1 dp", GOSSIP_SENDER_MAIN, 2005);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Tauren Female - Cost 1 dp", GOSSIP_SENDER_MAIN, 2006);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Blood Elf Male - Cost 1 dp", GOSSIP_SENDER_MAIN, 2007);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Blood Elf Female - Cost 1 dp", GOSSIP_SENDER_MAIN, 2008);
                 pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Return", GOSSIP_SENDER_MAIN, 9999);
                 pPlayer->PlayerTalkClass->SendGossipMenu(DEFAULT_GOSSIP_MESSAGE, pCreature->GetGUID());
                 return true;
@@ -203,11 +200,11 @@ class donationrewarder : public CreatureScript
                 break;
             case 3000:
                 pPlayer->PlayerTalkClass->ClearMenus();
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Sharpened Twilight Scale - Cost 5 DP", GOSSIP_SENDER_MAIN, 3001);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Charred Twilight Scale - Cost 5 DP", GOSSIP_SENDER_MAIN, 3002);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Glowing Twilight Scale - Cost 5 DP", GOSSIP_SENDER_MAIN, 3003);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Petrified Twilight Scale - Cost 5 DP", GOSSIP_SENDER_MAIN, 3004);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Deathbringer's Will - Cost 4 DP", GOSSIP_SENDER_MAIN, 3005);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Sharpened Twilight Scale - Cost 5 dp", GOSSIP_SENDER_MAIN, 3001);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Charred Twilight Scale - Cost 5 dp", GOSSIP_SENDER_MAIN, 3002);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Glowing Twilight Scale - Cost 5 dp", GOSSIP_SENDER_MAIN, 3003);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Petrified Twilight Scale - Cost 5 dp", GOSSIP_SENDER_MAIN, 3004);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Deathbringer's Will - Cost 4 dp", GOSSIP_SENDER_MAIN, 3005);
                 pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Return", GOSSIP_SENDER_MAIN, 9999);
                 pPlayer->PlayerTalkClass->SendGossipMenu(DEFAULT_GOSSIP_MESSAGE, pCreature->GetGUID());
                 return true;
@@ -230,19 +227,23 @@ class donationrewarder : public CreatureScript
             case 4000:
                 Reward(pPlayer,  pCreature, 500600, 1, 5);
                 pPlayer->CLOSE_GOSSIP_MENU();       //fix?
+                pPlayer->PlayerTalkClass->ClearMenus();
+                OnGossipHello(pPlayer, pCreature);
                 break;
             case 4001:
                 Reward(pPlayer,  pCreature, 500600, 4, 18);
-                pPlayer->CLOSE_GOSSIP_MENU();       //fix??
+                pPlayer->CLOSE_GOSSIP_MENU();       //fix?
+                pPlayer->PlayerTalkClass->ClearMenus();
+                OnGossipHello(pPlayer, pCreature);
                 break;
             case 5000:
                 pPlayer->PlayerTalkClass->ClearMenus();
                 //pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, "[Murloc Costume] - 16 VP", GOSSIP_SENDER_MAIN, 5001);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Character Rename - Cost 4 DP", GOSSIP_SENDER_MAIN, 5002);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Race Change - Cost 8 DP", GOSSIP_SENDER_MAIN, 5003);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Faction Change - Cost 13 DP", GOSSIP_SENDER_MAIN, 5004);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, "40x Commendation of Bravery - Cost 5 DP", GOSSIP_SENDER_MAIN, 5005);
-                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, "200x Champion's Seal - Cost 8 DP", GOSSIP_SENDER_MAIN, 5006);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Character Rename - Cost 4 dp", GOSSIP_SENDER_MAIN, 5002);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Race Change - Cost 8 dp", GOSSIP_SENDER_MAIN, 5003);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TAXI, "Faction Change - Cost 13 dp", GOSSIP_SENDER_MAIN, 5004);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, "40x Commendation of Bravery - Cost 5 dp", GOSSIP_SENDER_MAIN, 5005);
+                pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_MONEY_BAG, "200x Champion's Seal - Cost 8 dp", GOSSIP_SENDER_MAIN, 5006);
                 pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Return", GOSSIP_SENDER_MAIN, 9999);
                 pPlayer->PlayerTalkClass->SendGossipMenu(DEFAULT_GOSSIP_MESSAGE, pCreature->GetGUID());
                 return true;
@@ -270,8 +271,7 @@ class donationrewarder : public CreatureScript
                 OnGossipHello(pPlayer, pCreature);
                 break;
             }
-            pPlayer->PlayerTalkClass->ClearMenus();
-            OnGossipHello(pPlayer, pCreature);
+
             return true;
         }
 
